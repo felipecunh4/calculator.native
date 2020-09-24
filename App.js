f0d6eb73d283ccea8faf1d65fd8cb5f0a02d374c
Felipe Cunha <felipe.cunha@afya.com.br>
2020-09-24 13:21:28 -0300

    buttons style and click operations

--- App.js
+++ App.js
@@ -6,6 +6,19 @@ import Display from './scr/components/Display';
 
 const App = () => {
   const [displayValue, setDisplayValue] = useState(0);
+  // const [operation, setOperation] = useState('');
+
+  const addDigit = (n) => {
+    setDisplayValue(n);
+  };
+
+  const clearMemory = () => {
+    setDisplayValue(0);
+  };
+
+  const setOperation = (operation) => {
+    setDisplayValue(0);
+  };
 
   return (
     <>
@@ -14,23 +27,23 @@ const App = () => {
           <View style={styles.container}>
             <Display value={displayValue} />
             <View style={styles.buttons}>
-              <Button label="AC" />
-              <Button label="/" />
-              <Button label="7" />
-              <Button label="8" />
-              <Button label="9" />
-              <Button label="*" />
-              <Button label="4" />
-              <Button label="5" />
-              <Button label="6" />
-              <Button label="-" />
-              <Button label="1" />
-              <Button label="2" />
-              <Button label="3" />
-              <Button label="+" />
-              <Button label="0" />
-              <Button label="." />
-              <Button label="=" />
+              <Button label="AC" triple={true} onClick={clearMemory} />
+              <Button label="/" operation={true} onClick={setOperation} />
+              <Button label="7" onClick={addDigit} />
+              <Button label="8" onClick={addDigit} />
+              <Button label="9" onClick={addDigit} />
+              <Button label="*" operation={true} onClick={setOperation} />
+              <Button label="4" onClick={addDigit} />
+              <Button label="5" onClick={addDigit} />
+              <Button label="6" onClick={addDigit} />
+              <Button label="-" operation={true} onClick={setOperation} />
+              <Button label="1" onClick={addDigit} />
+              <Button label="2" onClick={addDigit} />
+              <Button label="3" onClick={addDigit} />
+              <Button label="+" operation={true} onClick={setOperation} />
+              <Button label="0" double={true} onClick={addDigit} />
+              <Button label="." onClick={addDigit} />
+              <Button label="=" operation={true} onClick={setOperation} />
             </View>
           </View>
         </ScrollView>
