a5d65a9fad8d54604c658dbc8ba12a1c7c13adce
Felipe Cunha <felipe.cunha@afya.com.br>
2020-09-29 17:01:22 -0300

    adding operation

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, Component} from 'react';
 import {SafeAreaView, StyleSheet, ScrollView, View} from 'react-native';
 
 import Button from './scr/components/Button';
@@ -12,73 +12,90 @@ const initialState = {
   current: 0,
 };
 
-const App = () => {
-  const [displayValue, setDisplayValue] = useState(initialState.displayValue);
-  const [operationValue, setOperationValue] = useState('');
-  const [clearDisplay, setClearDisplay] = useState(initialState.clearDisplay);
-  const [values, setValues] = useState(initialState.values);
-  const [current, setCurrent] = useState(initialState.current);
+export default class App extends Component {
+  state = {...initialState};
 
-  const addDigit = (n) => {
-    if (n === '.' && displayValue.includes('.')) return;
+  addDigit = (n) => {
+    const clearDisplay =
+      this.state.displayValue === '0' || this.state.clearDisplay;
 
-    const clear = displayValue == '0' || clearDisplay;
-    const currentValue = clear ? '' : displayValue;
-    const newDisplayValue = currentValue + n;
-    setDisplayValue(newDisplayValue);
-    setClearDisplay(false);
+    if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')) {
+      return;
+    }
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+    const displayValue = currentValue + n;
+    this.setState({displayValue, clearDisplay: false});
 
     if (n !== '.') {
       const newValue = parseFloat(displayValue);
-      const valuesInArray = [...values];
-      valuesInArray[current] = newValue;
-      setValues(valuesInArray);
+      const values = [...this.state.values];
+      values[this.state.current] = newValue;
+      this.setState({values});
     }
   };
 
-  const clearMemory = () => {
-    setDisplayValue(initialState.displayValue);
-    setOperationValue(initialState.operation);
-    setClearDisplay(initialState.clearDisplay);
-    setValues(initialState.values);
-    setCurrent(initialState.current);
+  clearMemory = () => {
+    this.setState({...initialState});
   };
 
-  const setOperation = (operation) => {
-    setDisplayValue(0);
+  setOperation = (operation) => {
+    if (this.state.current === 0) {
+      this.setState({operation, current: 1, clearDisplay: true});
+    } else {
+      const equals = operation === '=';
+      const values = [...this.state.values];
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`);
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+
+      values[1] = 0;
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        //clearDisplay: !equals,
+        clearDisplay: true,
+        values,
+      });
+    }
   };
 
-  return (
-    <>
-      <SafeAreaView>
-        <ScrollView>
-          <View style={styles.container}>
-            <Display value={displayValue} />
-            <View style={styles.buttons}>
-              <Button label="AC" triple={true} onClick={clearMemory} />
-              <Button label="/" operation={true} onClick={setOperation} />
-              <Button label="7" onClick={addDigit} />
-              <Button label="8" onClick={addDigit} />
-              <Button label="9" onClick={addDigit} />
-              <Button label="*" operation={true} onClick={setOperation} />
-              <Button label="4" onClick={addDigit} />
-              <Button label="5" onClick={addDigit} />
-              <Button label="6" onClick={addDigit} />
-              <Button label="-" operation={true} onClick={setOperation} />
-              <Button label="1" onClick={addDigit} />
-              <Button label="2" onClick={addDigit} />
-              <Button label="3" onClick={addDigit} />
-              <Button label="+" operation={true} onClick={setOperation} />
-              <Button label="0" double={true} onClick={addDigit} />
-              <Button label="." onClick={addDigit} />
-              <Button label="=" operation={true} onClick={setOperation} />
+  render() {
+    return (
+      <>
+        <SafeAreaView>
+          <ScrollView>
+            <View style={styles.container}>
+              <Display value={this.state.displayValue} />
+              <View style={styles.buttons}>
+                <Button label="AC" triple onClick={this.clearMemory} />
+                <Button label="/" operation onClick={this.setOperation} />
+                <Button label="7" onClick={this.addDigit} />
+                <Button label="8" onClick={this.addDigit} />
+                <Button label="9" onClick={this.addDigit} />
+                <Button label="*" operation onClick={this.setOperation} />
+                <Button label="4" onClick={this.addDigit} />
+                <Button label="5" onClick={this.addDigit} />
+                <Button label="6" onClick={this.addDigit} />
+                <Button label="-" operation onClick={this.setOperation} />
+                <Button label="1" onClick={this.addDigit} />
+                <Button label="2" onClick={this.addDigit} />
+                <Button label="3" onClick={this.addDigit} />
+                <Button label="+" operation onClick={this.setOperation} />
+                <Button label="0" double onClick={this.addDigit} />
+                <Button label="." onClick={this.addDigit} />
+                <Button label="=" operation onClick={this.setOperation} />
+              </View>
             </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -89,5 +106,3 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
 });
-
-export default App;
